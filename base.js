d9b8fa52712c81c56d42d39d798f58ea9a55508b
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2021-03-08 13:00:26 +0100

    funcs to handle nodes referring only to nodes

--- base.js
+++ base.js
@@ -56,60 +56,41 @@
       return all ? node.querySelectorAll(query) : node.querySelector(query);
     }
 
-    //TODO
-    // direct access to node
-    // return bool
-    allocMethod(mfix, root, node) {
-      const _ns = '_' + this.options.ns;
-      const root_isCompo = Compo.isCompo(root);
-      const node_isCompo = Compo.isCompo(node);
-
-      if (root_isCompo && node_isCompo) {
-        return root[mfix](node[_ns]);
-      } else if (root_isCompo) {
-        return !! root[_ns][mfix + 'Child'](node);
-      } else if (node_isCompo) {
-        return root[mfix + 'Child'](node[_ns]);
-      }
-
-      return root[mfix + 'Child'](node);
-    }
-
     // return bool
     appendNode(root, node) {
-      return this.allocMethod('append', root, node);
+      return !! root.appendChild(node);
     }
 
     // return bool
     prependNode(root, node) {
-      return this.allocMethod('prepend', root, node);
+      return !! root.prependChild(node);
     }
 
     // return bool
     removeNode(root, node) {
-      return this.allocMethod('remove', root, node);
+      return !! root.removeChild(node);
     }
 
     cloneNode(node, deep = false) {
-      return Compo.isCompo(node) ? node.clone(deep) : node.cloneNode(deep);
+      return node.cloneNode(deep);
     }
 
     hasAttr(node, attr) {
-      return Compo.isCompo(node) ? node.hasAttr(attr) : node.hasAttribute(attr);
+      return node.hasAttribute(attr);
     }
 
     getAttr(node, attr) {
-      return Compo.isCompo(node) ? node.getAttr(attr) : node.getAttribute(attr);
+      return node.getAttribute(attr);
     }
 
     // return undef
     setAttr(node, attr, value) {
-      Compo.isCompo(node) ? node.setAttr(attr, value) : node.setAttribute(attr, value);
+      node.setAttribute(attr, value);
     }
 
     // return undef
     delAttr(node, attr) {
-      Compo.isCompo(node) ? node.delAttr(attr) : node.removeAttribute(attr);
+      node.removeAttribute(attr);
     }
 
     binds(method) {
